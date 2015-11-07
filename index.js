adcee4ab3551dc6bda9ea10bf35359ed76026de1
Shaytan1986 <tetroxide@gmail.com>
2015-11-06 20:32:32 -0700

    first major addition

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
 // Module imports
 var express = require("express"),
-	mockData = require("./lib/mockData"),
+	mockData = require("./lib/mockData.js"),
 	handlebars = require("express-handlebars").create({
 		defaultLayout:"main"
 	}),
 	// A library of useful JS functions
-	util = require("./lib/util");
+	util = require("./lib/util.js"),
+	styles = require('./lib/styles.js');
 
 // Initialize and setup the app and stuff
 var app = express();
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 app.set("port", process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
 
+var oAlertData = {
+	alertItemId:util.req("ITEM", ""),
+	WSODISSUE:util.req("WSODISSUE", ""),
+	Symbol:util.req("Symbol", ""),
+	CompanyName:util.req("CompanyName", ""),
+	styles:styles.Styles,
+	dtNow:new Date().toDateString()
+};
 
-
+// This is the main heavy lifter. It does all the data fetching and assignment
 app.use(function(req, res, next) {
+
+	switch (oAlertData.alertItemId) {
+		case "1":
+			break;
+		case "2":
+			break;
+		case "3":
+			oAlertData.TRIGGER_PRICE = util.req("TRIGGER_PRICE", "");
+			oAlertData.TRIGGER_LIMIT = util.req("TRIGGER_LIMIT", "");
+			oAlertData.TRIGGER_VALUE = util.req("TRIGGER_VALUE", "");
+			break;
+	}
 	// If there is no partials object, create it
 	if (!res.locals.partials) res.locals.partials = {};
-	res.locals.partials.postData = mockData.PostData;
-	res.locals.AlertItemId = util.req("ITEM", "");
+	// This has to be at the locals level, because the dynamic lookup must find the variable here
+	res.locals.alertItemId = oAlertData.alertItemId;
+	// Not strictly necessary, but this sort of keeps with the model of our existing alert templates.
+	// Alternatively, I dont see why everything could'nt simply be appended to res.locals.
+	// My book talks about res.locals.partials as the partials namespace, but I assume you could use whatever's clever.
+	res.locals.oAlertData = oAlertData;
+
 	next();
 })
 app.get('/', function(req, res){
